94004650fc3cd5b17b2985e1a8ee5fff4a449b67
amoradi <aaron.moradi@daveramsey.com>
2017-05-06 15:30:14 -0500

    complete tests

--- server/spec.js
+++ server/spec.js
@@ -8,6 +8,8 @@ var expect = require('chai').expect;
 
 describe('[LIONS]', function(){
 
+  var id = 0;
+
   // get all lions
   it('should get all lions', function(done) {
     request(app)
@@ -22,53 +24,69 @@ describe('[LIONS]', function(){
   });
 
   // create 1 lion
-  it('should get all lions', function(done) {
+  it('should create 1 lion', function(done) {
+    var lion = {
+      name: 'Aaron',
+      pride: 'Moradi',
+      age: 32,
+      gender: 'male'
+    };
+
     request(app)
-      .post('/lions:id')
+      .post('/lions')
+      .send(lion)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(201)
       .end(function(err, resp) {
-        expect(resp.body).to.be.an('object');
+        var aaron = resp.body;
+
+        expect(aaron).to.be.an('object');
+        expect(aaron.name).to.eql('Aaron');
+        //expect(aaron.age).to.eql(32);
         done();
       })
   });
 
   // get 1 lion
-  it('should get all lions', function(done) {
+  it('should get 1 lion', function(done) {
     request(app)
-      .get('/lions')
+      .get('/lions/1')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, resp) {
-        expect(resp.body).to.be.an('array');
+        expect(resp.body).to.be.an('object');
+        expect(resp.body.id).to.equal("1");
         done();
       })
   });
 
   // update 1 lion
-  it('should get all lions', function(done) {
+  it('should update 1 lion', function(done) {
     request(app)
-      .get('/lions')
+      .put('/lions/1')
+      .send({name: "Cameron"})
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, resp) {
-        expect(resp.body).to.be.an('array');
+        expect(resp.body).to.be.an('object');
+        expect(resp.body.name).to.equal('Cameron');
         done();
       })
   });
 
   // delete 1 lion
-  it('should get all lions', function(done) {
+  it('should delete 1 lion', function(done) {
     request(app)
-      .get('/lions')
+      .delete('/lions/1')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, resp) {
-        expect(resp.body).to.be.an('array');
+        expect(resp.body).to.be.an('object');
+        expect(resp.body.name).to.equal('Cameron');
         done();
       })
   });
