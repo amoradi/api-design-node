ff6a7364de10e9655b672c7845a1c929c78a91b2
amoradi <aaron.moradi@daveramsey.com>
2017-05-06 14:59:49 -0500

    stub tests

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-##Testing our app
+## Slides
+http://fem-node-api.netlify.com/
+
+## Testing our app
 
 We're going to use mocha and chai along with supertest to runt inegration test against our api. **Be sure to gloably install mocha**. `npm i -g mocha`. 
 
--- server/spec.js
+++ server/spec.js
@@ -8,6 +8,7 @@ var expect = require('chai').expect;
 
 describe('[LIONS]', function(){
 
+  // get all lions
   it('should get all lions', function(done) {
     request(app)
       .get('/lions')
@@ -19,6 +20,60 @@ describe('[LIONS]', function(){
         done();
       })
   });
+
+  // create 1 lion
+  it('should get all lions', function(done) {
+    request(app)
+      .post('/lions:id')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end(function(err, resp) {
+        expect(resp.body).to.be.an('object');
+        done();
+      })
+  });
+
+  // get 1 lion
+  it('should get all lions', function(done) {
+    request(app)
+      .get('/lions')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, resp) {
+        expect(resp.body).to.be.an('array');
+        done();
+      })
+  });
+
+  // update 1 lion
+  it('should get all lions', function(done) {
+    request(app)
+      .get('/lions')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, resp) {
+        expect(resp.body).to.be.an('array');
+        done();
+      })
+  });
+
+  // delete 1 lion
+  it('should get all lions', function(done) {
+    request(app)
+      .get('/lions')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, resp) {
+        expect(resp.body).to.be.an('array');
+        done();
+      })
+  });
+
+
 });
 
 
