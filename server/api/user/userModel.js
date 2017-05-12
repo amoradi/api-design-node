6e0f9d82258d081cc648139ee55a74ff73f640e3
amoradi <aaron.moradi@daveramsey.com>
2017-05-12 07:27:27 -0500

    step-8 solution

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -2,7 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  posts [
+    {
+      ref: 'posts',
+      type: Schema.Types.ObjectId
+    }
+  ]
 });
 
 module.exports = mongoose.model('user', UserSchema);
